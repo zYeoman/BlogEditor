26c74c3b2b92b57f8edaad87e757f368f9661378
Yongwen Zhuang <zyeoman@163.com>
2019-09-23 20:25:30 +0800

    tmp

--- editor/editor.js
+++ editor/editor.js
@@ -1,87 +1,109 @@
 /*
  * editor.js
- * Copyright (C) 2019 Yongwen Zhuang <zeoman@163.com>
+ * Copyright (C) 2019 Yongwen Zhuang <zyeoman@163.com>
  *
  * Distributed under terms of the MIT license.
  */
-(function () {
-  'use strict'
-  var LOCAL_STORAGE = 'BlogEditorContent'
-  var myTextarea = document.getElementById('md')
+
+var CodeMirror = require('codemirror/lib/codemirror')
+var HyperMD = require('hypermd')
+var PinyinMatch = require('pinyin-match')
+var Swal = require('sweetalert2')
+
+window.HyperMD = HyperMD
+window.CodeMirror = CodeMirror
+
+// Code Highlighting
+require('codemirror/mode/htmlmixed/htmlmixed') // for embedded HTML
+require('codemirror/mode/stex/stex') // for Math TeX Formular
+require('codemirror/mode/yaml/yaml') // for Front Matters
+
+require('hypermd/powerpack/fold-math-with-katex')
+require('hypermd/powerpack/paste-with-turndown')
+require('turndown-plugin-gfm')
+
+var LOCAL_STORAGE = 'BlogEditorContent'
+var myTextarea = document.getElementById('md')
+if (window.location.pathname === '/') {
+  myTextarea.value = window.localStorage[LOCAL_STORAGE] || ''
+}
+var editor = HyperMD.fromTextArea(myTextarea, {
+  // for code fence highlighting
+  hmdModeLoader: 'https://cdn.jsdelivr.net/npm/codemirror/',
+  mode: {
+    name: 'hypermd',
+    hashtag: true
+  },
+  lineNumbers: false,
+  theme: 'hypermd-light-transparent'
+})
+editor.on('change', e => {
   if (window.location.pathname === '/') {
-    myTextarea.value = window.localStorage[LOCAL_STORAGE] || ''
+    window.localStorage[LOCAL_STORAGE] = editor.getValue()
   }
-  var editor = window.HyperMD.fromTextArea(myTextarea, {
-    // for code fence highlighting
-    hmdModeLoader: 'https://cdn.jsdelivr.net/npm/codemirror/'
-  })
-  editor.on('change', e => {
-    if (window.location.pathname === '/') {
-      window.localStorage[LOCAL_STORAGE] = editor.getValue()
+})
+var saved = document.getElementById('saved')
+window.onbeforeunload = function () {
+  if (saved.innerText !== 'Saved!') {
+    upload()
+  }
+}
+function upload () {
+  var reg = /date: \d*-\d*-\d*.*/
+  var localTimeString = localeTime()
+  postAjax(
+    window.location.href,
+    { data: editor.getValue().replace(reg, 'date: ' + localTimeString) },
+    responseText => {
+      Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1000
+      }).fire({
+        type: 'success',
+        title: responseText
+      })
+      saved.innerText = 'Saved!'
     }
-  })
-  var saved = document.getElementById('saved')
-  window.onbeforeunload = function () {
-    if (saved.innerText !== 'Saved!') {
-      upload()
-    }
-  }
-  function upload () {
-    var reg = /date: \d*-\d*-\d*.*/
-    var localTimeString = localeTime()
-    postAjax(
-      window.location.href,
-      { data: editor.getValue().replace(reg, 'date: ' + localTimeString) },
-      responseText => {
-        window.Swal.mixin({
-          toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 1000
-        }).fire({
-          type: 'success',
-          title: responseText
-        })
-        saved.innerText = 'Saved!'
-      }
-    )
-  }
-  editor.on('change', () => {
-    saved.innerText = 'Unsaved'
-  })
-  editor.on(
-    'change',
-    debounce((cm, change) => {
-      upload()
-    }, 6000)
   )
+}
+editor.on('change', () => {
+  saved.innerText = 'Unsaved'
+})
+editor.on(
+  'change',
+  debounce((cm, change) => {
+    upload()
+  }, 6000)
+)
 
-  function localeTime () {
-    var tzoffset = new Date().getTimezoneOffset() * 60000 // offset in milliseconds
-    return (
-      new Date(Date.now() - tzoffset)
-        .toISOString()
-        .slice(0, -5)
-        .replace('T', ' ') + ' +0800'
-    )
-  }
+function localeTime () {
+  var tzoffset = new Date().getTimezoneOffset() * 60000 // offset in milliseconds
+  return (
+    new Date(Date.now() - tzoffset)
+      .toISOString()
+      .slice(0, -5)
+      .replace('T', ' ') + ' +0800'
+  )
+}
 
-  // https://javascript.ruanyifeng.com/advanced/timer.html
-  function debounce (fn, delay) {
-    var timer = null // 声明计时器
-    return function () {
-      var context = this
-      var args = arguments
-      clearTimeout(timer)
-      timer = setTimeout(function () {
-        fn.apply(context, args)
-      }, delay)
-    }
+// https://javascript.ruanyifeng.com/advanced/timer.html
+function debounce (fn, delay) {
+  var timer = null // 声明计时器
+  return function () {
+    var context = this
+    var args = arguments
+    clearTimeout(timer)
+    timer = setTimeout(function () {
+      fn.apply(context, args)
+    }, delay)
   }
-  // https://plainjs.com/javascript/ajax/send-ajax-get-and-post-requests-47/
-  // TODO: 用fetch更好
-  function postAjax (url, data, success) {
-    var params =
+}
+// https://plainjs.com/javascript/ajax/send-ajax-get-and-post-requests-47/
+// TODO: 用fetch更好
+function postAjax (url, data, success) {
+  var params =
       typeof data === 'string'
         ? data
         : Object.keys(data)
@@ -90,92 +112,92 @@
           })
           .join('&')
 
-    var xhr = window.XMLHttpRequest
-      ? new window.XMLHttpRequest()
-      : new window.ActiveXObject('Microsoft.XMLHTTP')
-    xhr.open('POST', url)
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState > 3 && xhr.status === 200) {
-        success(xhr.responseText)
-      }
-    }
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    xhr.send(params)
-    return xhr
-  }
-  // and that's all
-  // now you get a `editor` and you can do whatever you want
-  editor.setSize(null, '100%') // set height
-  editor.focus()
-
-  // TODO:
-  // for debugging
-  window.editor = editor
-  window.cm = editor
-
-  // load_and_update_editor(demo_filename)
-
-  document.getElementById('search').oninput = function () {
-    var text = document.getElementById('search').value.toLowerCase()
-    var posts = document.getElementsByClassName('post-item')
-    for (var i = 0, len = posts.length; i < len; i++) {
-      var str = posts[i].innerText
-      posts[i].hidden = !window.PinyinMatch.match(str, text)
+  var xhr = window.XMLHttpRequest
+    ? new window.XMLHttpRequest()
+    : new window.ActiveXObject('Microsoft.XMLHTTP')
+  xhr.open('POST', url)
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState > 3 && xhr.status === 200) {
+      success(xhr.responseText)
     }
   }
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+  xhr.send(params)
+  return xhr
+}
+// and that's all
+// now you get a `editor` and you can do whatever you want
+editor.setSize(null, '100%') // set height
+editor.focus()
 
-  // Toggle
-  editor.hyper = true
-  document.getElementById('toggle').onclick = function () {
-    if (editor.hyper) {
-      window.HyperMD.switchToNormal(editor)
-      editor.hyper = false
-    } else {
-      window.HyperMD.switchToHyperMD(editor)
-      editor.hyper = true
-    }
+// TODO:
+// for debugging
+window.editor = editor
+window.cm = editor
+
+// load_and_update_editor(demo_filename)
+
+document.getElementById('search').oninput = function () {
+  var text = document.getElementById('search').value.toLowerCase()
+  var posts = document.getElementsByClassName('post-item')
+  for (var i = 0, len = posts.length; i < len; i++) {
+    var str = posts[i].innerText
+    posts[i].hidden = !PinyinMatch.match(str, text)
   }
+}
 
-  // New File
-  document.getElementById('create').onclick = function () {
-    window.Swal.mixin({
-      input: 'text',
-      confirmButtonText: 'Next &rarr;',
-      showCancelButton: true,
-      progressSteps: ['1', '2', '3']
-    })
-      .queue([
-        {
-          title: '文件名',
-          text: '输入文件名'
-        },
-        '标题',
-        {
-          title: '分类',
-          input: 'select',
-          inputOptions: {
-            法: '法',
-            理: '理',
-            器: '器',
-            用: '用',
-            杂: '杂'
-          }
+// Toggle
+editor.hyper = true
+document.getElementById('toggle').onclick = function () {
+  if (editor.hyper) {
+    HyperMD.switchToNormal(editor)
+    editor.hyper = false
+  } else {
+    HyperMD.switchToHyperMD(editor)
+    editor.hyper = true
+  }
+}
+
+// New File
+document.getElementById('create').onclick = function () {
+  Swal.mixin({
+    input: 'text',
+    confirmButtonText: 'Next &rarr;',
+    showCancelButton: true,
+    progressSteps: ['1', '2', '3']
+  })
+    .queue([
+      {
+        title: '文件名',
+        text: '输入文件名'
+      },
+      '标题',
+      {
+        title: '分类',
+        input: 'select',
+        inputOptions: {
+          法: '法',
+          理: '理',
+          器: '器',
+          用: '用',
+          杂: '杂'
         }
-      ])
-      .then(result => {
-        if (result.value) {
-          var path = result.value[0]
-          var title = result.value[1]
-          var category = result.value[2]
-          var current = localeTime()
-          var filename =
+      }
+    ])
+    .then(result => {
+      if (result.value) {
+        var path = result.value[0]
+        var title = result.value[1]
+        var category = result.value[2]
+        var current = localeTime()
+        var filename =
             '/' + current.slice(0, 10) + '-' + path.replace(/ /g, '-') + '.md'
-          var url = document.location.href.replace(
-            /\/(\d{4}-\d{2}-\d{2}-.*\.md)?$/,
-            filename
-          )
-          var str =
+        var url = document.location.href.replace(
+          /\/(\d{4}-\d{2}-\d{2}-.*\.md)?$/,
+          filename
+        )
+        var str =
             '---\nlayout: post\ntitle: ' +
             title +
             '\ncategory: ' +
@@ -185,45 +207,44 @@
             '\ncreate: ' +
             current +
             '\ntags: \n  - \n---\n\n- TOC\n{:toc}'
-          postAjax(url, { data: str, action: 'Save' }, responseText => {
-            var type = 'success'
-            if (responseText !== 'Save Success!') {
-              type = 'error'
-            }
-            window.Swal.fire(responseText, '', type).then(result => {
-              window.location.href = url
-            })
+        postAjax(url, { data: str, action: 'Save' }, responseText => {
+          var type = 'success'
+          if (responseText !== 'Save Success!') {
+            type = 'error'
+          }
+          Swal.fire(responseText, '', type).then(result => {
+            window.location.href = url
           })
-        }
-      })
-  }
-  // Delete File
-  document.getElementById('delete').onclick = function () {
-    window.Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then(result => {
-      if (result.value) {
-        postAjax(window.location.href, { action: 'Delete' }, function () {
-          window.Swal.fire('Deleted!', 'Your file has been deleted.', 'success')
         })
       }
     })
-  }
+}
+// Delete File
+document.getElementById('delete').onclick = function () {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then(result => {
+    if (result.value) {
+      postAjax(window.location.href, { action: 'Delete' }, function () {
+        Swal.fire('Deleted!', 'Your file has been deleted.', 'success')
+      })
+    }
+  })
+}
 
-  // Preview Tex Math formula
-  // @see demo/math-preview.js
-  // init_math_preview(editor)
+// Preview Tex Math formula
+// @see demo/math-preview.js
+// init_math_preview(editor)
 
-  // Watch editor and generate TOC
-  // @see demo/toc.js
-  // init_toc(editor)
+// Watch editor and generate TOC
+// @see demo/toc.js
+// init_toc(editor)
 
-  // @see demo/lab.js
-  // init_lab(editor)
-})()
+// @see demo/lab.js
+// init_lab(editor)
